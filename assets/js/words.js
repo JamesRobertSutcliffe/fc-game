aaa4e896e6af94cd585fd046e24c382f2341047f
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-06-01 21:06:41 +0100

    Module created for word object / three letter non anagram words gathered

---
+++ assets/js/words.js
@@ -0,0 +1,24 @@
+const words = {
+    "three": ["ant",
+        "bag",
+        "cup",
+        "dry",
+        "egg",
+        "fan",
+        "gem",
+        "hot",
+        "joy",
+        "kit",
+        "lip",
+        "oak",
+        "pig",
+        "sun"],
+    "four": [],
+    "five": [],
+    "six": [],
+    "seven": [],
+    "eight": [],
+    "nine": []
+};
+
+export default words;
