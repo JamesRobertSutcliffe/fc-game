141305eebf30e3245eea3f250b4d25111994e53b
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-06-29 19:35:33 +0100

    Scores and name successfully rendering

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+const nameColumn = document.getElementById('name')
+const scoreColumn = document.getElementById('score')
+
+let scoresObject = JSON.parse(localStorage.getItem("scores"))
+
+console.log(scoresObject)
+
+for (x in scoresObject) {
+    let score = scoresObject[x]
+    const nameText = document.createElement('h3');
+    const scoreText = document.createElement('h3');
+    nameText.textContent = x;
+    scoreText.textContent = score;
+    nameColumn.append(nameText);
+    scoreColumn.append(scoreText);
+}
